dea15ce7cd8e5ad9d6feece1da42aaf3da283e89
Antonio Pires <tonan@outlook.com.br>
2024-10-03 06:14:23 -0300

    Removing person with DELETE

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -36,4 +36,10 @@ public class PessoaResource {
         return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.deleteById(codigo);
+    }
+
 }
