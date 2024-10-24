f3a5000ac65ddf37ead9631154acf71971c45e1e
Antonio Pires <tonan@outlook.com.br>
2024-10-24 07:05:31 -0300

    Creating the Contact entity to support master-detail

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -1,9 +1,20 @@
 package com.example.algamoney.api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
@@ -22,12 +33,24 @@ public class Pessoa {
     @NotNull
     private Boolean ativo;
 
+    @Valid
+    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
+    private List<Contato> contatos;
+
     public Long getCodigo() {
         return codigo;
     }
 
     public void setCodigo(Long codigo) {
         this.codigo = codigo;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contato> contatos) {
+        this.contatos = contatos;
     }
 
     public String getNome() {
