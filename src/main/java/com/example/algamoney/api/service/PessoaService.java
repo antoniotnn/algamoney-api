2d81052b8eab2d1abddb170d2cc496083aa1d19f
Antonio Pires <tonan@outlook.com.br>
2024-10-24 07:35:44 -0300

    Using the orphanRemoval property

--- src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -22,12 +22,13 @@ public class PessoaService {
     public Pessoa atualizar(Long codigo, Pessoa pessoa) {
         Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
 
-        pessoa.getContatos().forEach(c -> c.setPessoa(pessoa));
+        pessoaSalva.getContatos().clear();
+        pessoaSalva.getContatos().addAll(pessoa.getContatos());
+        pessoaSalva.getContatos().forEach(c -> c.setPessoa(pessoaSalva));
 
-        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo", "contatos");
         return pessoaRepository.save(pessoaSalva);
     }
-
 
     public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
         Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
