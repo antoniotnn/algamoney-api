6247608da98cbcb5224b9a394e2c6920ca243812
Antonio Pires <tonan@outlook.com.br>
2024-10-24 06:29:06 -0300

    Scheduling email sending

--- src/main/java/com/example/algamoney/api/mail/Mailer.java
+++ src/main/java/com/example/algamoney/api/mail/Mailer.java
@@ -1,8 +1,10 @@
 package com.example.algamoney.api.mail;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -13,6 +15,9 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Usuario;
 
 @Component
 public class Mailer {
@@ -41,10 +46,25 @@ public class Mailer {
 //		System.out.println("Terminado o envio de e-mail...");
 //	}
 
+	public void avisarSobreLancamentosVencidos(
+			List<Lancamento> vencidos, List<Usuario> destinatarios) {
+		Map<String, Object> variaveis = new HashMap<>();
+		variaveis.put("lancamentos", vencidos);
+
+		List<String> emails = destinatarios.stream()
+				.map(u -> u.getEmail())
+				.collect(Collectors.toList());
+
+		this.enviarEmail("testes.algaworks@gmail.com",
+				emails,
+				"Lançamentos vencidos",
+				"mail/aviso-lancamentos-vencidos",
+				variaveis);
+	}
+
 	public void enviarEmail(String remetente,
 							List<String> destinatarios, String assunto, String template,
 							Map<String, Object> variaveis) {
-
 		Context context = new Context(new Locale("pt", "BR"));
 
 		variaveis.entrySet()
