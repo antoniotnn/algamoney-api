a996600ed8b380070ffd189bca8f9f01fd0e1a3e
Antonio Pires <tonan@outlook.com.br>
2024-10-28 06:54:13 -0300

    phone is not required anymore in contact table. Changing migration

--- src/main/resources/db/migration/V05__criar_tabela_contato.sql
+++ src/main/resources/db/migration/V05__criar_tabela_contato.sql
@@ -3,7 +3,7 @@ CREATE TABLE contato (
 	codigo_pessoa BIGINT(20) NOT NULL,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(100) NOT NULL,
-	telefone VARCHAR(20) NOT NULL,
+	telefone VARCHAR(20),
   FOREIGN KEY (codigo_pessoa) REFERENCES pessoa(codigo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
